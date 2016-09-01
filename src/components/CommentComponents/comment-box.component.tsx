cb39c952d7f708dd0689520aba2fe2e6eb03254e
Mihail <mihail.davitkovski@endava.com>
2016-09-01 17:02:33 +0200

    redux in comments component

--- src/components/CommentComponents/comment-box.component.tsx
+++ src/components/CommentComponents/comment-box.component.tsx
@@ -13,43 +13,29 @@ import CommentActions  from "../../actions/CommentActions";
 
 import { IComments } from "./model/IComments";
 
-const mapStateToProps = (state: IComments) => {
-  return {
-    comments: state.comments
-  }
-}
-
-const mapDispatchToProps = (dispatch:any) => {
-  return {
-    onCommentSubmit: (comment: IComment) => {
-      //dispatch(null)
-    }
-  }
-}
-
-@connect(mapStateToProps, mapDispatchToProps)
-export const CommentBox: React.StatelessComponent<IComments> = (props) => (
-    <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList comments={this.props.comments}/>
-                <br/><br/>
-                <CommentForm  onCommentSubmit={() => handleCommentSubmit()}/>
-                <br/><br/><br/>
-                <div>
-                    <a href="/search"><b>Go to Search page</b></a>
-                </div>
-            </div>
-);
-
-@connect(mapStateToProps, mapDispatchToProps)
-export class CommentBox extends React.Component<IComments, void>
+interface CommentBoxProps
 {
-    constructor(props:IComments)
+    comments: Array<IComment>,
+    onCommentSubmit: any
+}
+
+const mapStateToProps = (state: IComments) => ({comments: state.comments});
+  
+const mapDispatchToProps = (dispatch: any) => ({
+    onCommentSubmit: (comment: IComment) => {
+      dispatch(comment);
+    }
+ });
+
+
+@connect(mapStateToProps, mapDispatchToProps)
+export class CommentBox extends React.Component<CommentBoxProps, {}>
+{
+    constructor(props: CommentBoxProps)
     {
         super(props);
     }
     
-
     render()
     {
         return (
