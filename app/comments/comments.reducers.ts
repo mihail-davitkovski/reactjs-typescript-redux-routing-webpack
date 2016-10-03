0baf036fbe649a2a821bbd1f64bf498021f5f9b3
Mihail <mihail.davitkovski@endava.com>
2016-10-03 17:04:58 +0200

    Refactoring

--- app/comments/comments.reducers.ts
+++ app/comments/comments.reducers.ts
@@ -1,6 +1,11 @@
+
+
 import {IComment} from "../comments/model/IComment";
 
 import {CommentActionTypes} from "../comments/action-creators/comment-action.types";
+
+
+import  * as objectAssign from "object-assign";
 
 const initialCommentState = {
     comments: Array<IComment>()
@@ -10,7 +15,10 @@ const commentsReducers = (state = initialCommentState, action:any) => {
   switch (action.type) {
     case CommentActionTypes.ADD_COMMENT:
       return Object.assign({}, state, {
-        comments: action.data
+          comments: [
+            ...state.comments,
+            action.data.comment
+          ]
       })
     default:
       return state
--- index.tsx
+++ index.tsx
@@ -16,16 +16,6 @@ import { CommentBox } from "./app/comments/comment-box.component";
  
  let store = createStore(reducers);
 
-//import { FilterableProductTable } from "./src/Components/SearchComponents/FilterableProductTable";
-
-/*ReactDom.render((
-  <ReactRouter.Router history={ReactRouter.browserHistory}>
-    <ReactRouter.Route path="/" component={CommentBox}/>
-    <ReactRouter.Route path="search" component={FilterableProductTable}/>
-    <ReactRouter.Route path="comments" component={CommentBox}/>
-  </ReactRouter.Router>
-), document.getElementById('content'))*/
-
 ReactDom.render(
   <Provider store={store}>
     <ReactRouter.Router history={history}>
