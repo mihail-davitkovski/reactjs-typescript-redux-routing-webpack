5c0da883aef2423988ad6bb2e19f4e0d0684e45f
Mihail <mihail.davitkovski@endava.com>
2017-01-13 15:18:49 +0100

    Use lite server

--- app/comments/action-creators/comment-action.creators.ts
+++ app/comments/action-creators/comment-action.creators.ts
@@ -30,7 +30,7 @@ const receiveCommentsFinsished = (comments: Array<IComment>) => (
 
 export const getAllComments = () => {
   return (dispatch: any) => {
-    return fetch("http://localhost:8080/comments.json")
+    return fetch("http://localhost:3000/comments.json")
       .then(response => response.json())
       .then(result => dispatch(receiveCommentsFinsished(result.comments)))
   }
